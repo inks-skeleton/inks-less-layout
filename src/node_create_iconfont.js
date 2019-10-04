1ab31226225e6286a6cd968ca36d2095e9555b90
YanEr <lx@qcmail.cn>
2019-10-04 18:03:40 +0800

    1.新增多种hover特效mixin 2.新增弹性盒子布局样式 3.图标创建路径改为自动识别 4.增加字体样式更新修改

--- src/node_create_iconfont.js
+++ src/node_create_iconfont.js
@@ -2,11 +2,12 @@ if (!process.argv[2]) {
   console.error('抱歉！不存在可编译的css文件，请使用 "npm run test:icon [<args>]"来添加编译文件路径')
   return
 }
-const url = process.argv[2]
-const outUrl = 'src/component/_fontIcon.less'
-
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
+
+const url = process.argv[2]
+const outUrl = 'src/assets/styles/dev/component/_fontIcon.less'
 
 http.get('http:' + url, function (response) {
   response.setEncoding('binary') // 二进制binary
@@ -29,7 +30,7 @@ http.get('http:' + url, function (response) {
     fs.writeFile(outUrl, result, function () {
       console.log('\x1B[33m%s\x1B[39m', 'Icon Less 文件创建成功！')
       console.log('\x1B[32m%s\x1B[39m', '请打开文件路径，进行保存，以便less成功被编译 \r\n')
-      console.log('\x1B[32m%s\x1B[39m', '[文件路径] E:/DevSpace/YanErGithub.space/LayoutSimple/' + outUrl + '\r\n')
+      console.log('\x1B[32m%s\x1B[39m', '[文件路径]'+  path.resolve(outUrl) + '\r\n')
     })
   })
 })
