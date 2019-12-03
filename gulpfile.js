3479fd5cdeae169ed2b98792e6226b7f1da27cae
YanEr <lx@qcmail.cn>
2019-12-03 23:09:43 +0800

    迭代更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,49 @@
 // 引入npm资源
-const path = require('path');
-const gulp = require('gulp');
+const fs = require('fs')
+const path = require('path')
+const gulp = require('gulp')
 const gulpRename = require('gulp-rename')
-const gulpLess = require('gulp-less');
-const gulpMinifyCSS = require('gulp-minify-css');
-const del = require('del');
+const gulpLess = require('gulp-less')
+const lessAutoprefix = require('less-plugin-autoprefix')
+const gulpMinifyCSS = require('gulp-minify-css')
+const del = require('del')
+const package = JSON.parse(fs.readFileSync('./package.json'))
+
+const autoprefix = new lessAutoprefix({ browsers: ['last 2 versions'] })
+const outFile = path.join(__dirname, 'dist')
+const devLess = path.join(__dirname, 'src/LayoutSimple.less')
 
 // 清理输出文件夹
 gulp.task('clean-dist', (done) => {
-	return del([config.output.file + '/**/*'], done);
-});
+	return del([outFile], done)
+})
 
 // 编译less样式为css
-gulp.task('less', () => {
+gulp.task('less2css', () => {
 	return gulp
-		.src('./src/*.less')
-		.pipe(gulpLess())
-		.pipe(gulp.dest('./dist'));
+		.src(devLess)
+		.pipe(gulpLess({
+      plugins: [autoprefix]
+    }))
+    .pipe(gulpRename((path) => {
+			path.basename += '.' + package.version
+		}))
+		.pipe(gulp.dest(outFile))
 });
 
-gulp.task('less', () => {
+// 编译less样式为min.css
+gulp.task('less2mincss', () => {
 	return gulp
-		.src('./src/*.less')
-		.pipe(gulpLess())
+		.src(devLess)
+		.pipe(gulpLess({
+      plugins: [lessAutoprefix]
+    }))
 		.pipe(gulpMinifyCSS())
 		.pipe(gulpRename((path) => {
-			path.basename += "-min"
+			path.basename += '.' + package.version + '.min'
 		}))
-		.pipe(gulp.dest('./dist'));
-});
+		.pipe(gulp.dest(outFile))
+})
 
 // 打包
-gulp.task('build', gulp.series('clean-dist', gulp.parallel('less')));
+gulp.task('build', gulp.series('clean-dist', gulp.parallel('less2css', 'less2mincss')))
